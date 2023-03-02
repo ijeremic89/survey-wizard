7a5e2a2c944ace41ab7173614f2ff01a326ef284
ijeremic <ijeremic89@gmail.com>
2023-03-02 18:34:34 +0100

    Fix @RequestMapping for UserController

--- src/main/kotlin/com/example/surveyWizard/controllers/UserController.kt
+++ src/main/kotlin/com/example/surveyWizard/controllers/UserController.kt
@@ -2,23 +2,20 @@ package com.example.surveyWizard.controllers
 
 import com.example.surveyWizard.dtos.UserDTO
 import com.example.surveyWizard.services.UserService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/users")
 class UserController(
     private val userService: UserService
 ) {
-    @GetMapping("/{id}")
-    fun findById(@PathVariable("id") id: Int): UserDTO? {
+    @RequestMapping("/{id}", method = [RequestMethod.GET])
+    fun findById(@PathVariable id: Int): UserDTO? {
         return userService.findById(id)
     }
 
-    @GetMapping("/{email}")
-    fun findByEmail(@PathVariable("email") email: String) : UserDTO? {
+    @RequestMapping(method = [RequestMethod.GET])
+    fun findByEmail(@RequestParam email: String) : UserDTO? {
         return userService.findByEmail(email)
     }
 
